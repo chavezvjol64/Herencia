64e97f2c58572670fe38b24a6c44a8d17e612df2
chavezvjol64 <51514441+chavezvjol64@users.noreply.github.com>
2019-06-10 13:57:32 -0700

    Add files via upload

---
+++ Herencia/Herencia/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    public class clsClientes
+    {
+        private int _IdcCliente;
+        public int IdcCliente { get => _IdcCliente; set => _IdcCliente = value; }
+
+        private string _Nombres;
+
+        public string Nombres { get => _Nombres; set => _Nombres = value; }
+
+        private string _Apellidos;
+
+        public string Apellidos { get => _Apellidos; set => _Apellidos = value; }
+    }
+}
---
+++ Herencia/Herencia/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Objetos de la clase
+            clsClientesVentas Cliente;
+            //Creando instancia
+            Cliente = new clsClientesVentas();
+
+            Cliente.IdcCliente = 123;
+            Cliente.Apellidos = "Pon Atencion";
+            Cliente.Nombres = "jose Luis";
+            Cliente.RFC = "DSNMFSDF";
+            Cliente.Direccion = "Inserte aqui la direccion de CESCA";
+            Cliente.Municipio = "Tijuana es un municipio? :/";
+            Cliente.EsCredito = false;
+
+            Console.WriteLine(Cliente.IdcCliente + " " + Cliente.Apellidos + " " + Cliente.Nombres);
+            Console.WriteLine(Cliente.Direccion + " " + Cliente.Municipio);
+
+            if (Cliente.EsCredito)
+                Console.WriteLine("El cliente tiene credito");
+            else
+                Console.WriteLine("El cliente no tiene credito");
+            Console.ReadKey();
+
+        }
+    }
+}
